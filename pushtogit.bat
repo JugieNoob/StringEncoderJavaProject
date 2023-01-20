1ff53c73c5b770ff0d4e8331498a10ae401bedf4
JugieNoob <jurgenarna@gmail.com>
2023-01-20 22:20:01 +0100

    Init Commit

--- pushtogit.bat
+++ pushtogit.bat
@@ -3,5 +3,5 @@ git init
 git add .
 set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
-git remote add origin https://github.com/JugieNoob/Sorting-Struggle.git
+git remote add origin https://github.com/JugieNoob/StringEncoderJavaProject.git
 git push -u -f origin master
