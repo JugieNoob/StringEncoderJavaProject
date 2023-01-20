65c6449c7f88879df8a536e9e19cfd3b74d6dfa6
JugieNoob <jurgenarna@gmail.com>
2023-01-20 22:22:38 +0100

    a

--- pushtogit.bat
+++ pushtogit.bat
@@ -3,5 +3,6 @@ git init
 git add .
 set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
+git remote set-url --add -push origin https://github.com/JugieNoob/StringEncoderJavaProject.git
 git remote add origin https://github.com/JugieNoob/StringEncoderJavaProject.git
 git push -u -f origin master
