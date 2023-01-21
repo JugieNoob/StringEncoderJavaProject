ce78d992f75a129fd38dbfef3d6cefa305cbc7c7
JugieNoob <jurgenarna@gmail.com>
2023-01-21 12:25:32 +0100

    test

--- pushtogit.bat
+++ pushtogit.bat
@@ -4,7 +4,7 @@ git remote set-url --add --push origin https://github.com/JugieNoob/StringEncode
 git add .
 set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
-git push origin main
+git push origin master
 echo "Committed to Github!"
 echo "Press ENTER to close this window!"
 set /p EnterToClose=
