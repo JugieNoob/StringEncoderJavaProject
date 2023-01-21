d1ca88a3d54e9bc542a3ddb5027d2d23d32aa4ff
JugieNoob <jurgenarna@gmail.com>
2023-01-21 10:19:22 +0100

    Added Dropdown menu WIP

--- pushtogit.bat
+++ pushtogit.bat
@@ -1,6 +1,7 @@
 @echo off
 git init
 git add .
+git pull
 set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
 git remote set-url --add --push origin https://github.com/JugieNoob/StringEncoderJavaProject.git
