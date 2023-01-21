9a5196755a8de70fb573fa5d907b1d62d03a1942
JugieNoob <jurgenarna@gmail.com>
2023-01-21 12:11:58 +0100

    Test

--- pushtogit.bat
+++ pushtogit.bat
@@ -1,9 +1,9 @@
 @echo off
 git init
+git remote set-url --add --push origin https://github.com/JugieNoob/StringEncoderJavaProject.git
 git add .
 set /p NameOfCommit=What what you like to name your commit?
 git commit -m "%NameOfCommit%"
-git remote set-url --add --push origin https://github.com/JugieNoob/StringEncoderJavaProject.git
 git pull --rebase origin main
 git push -u origin origin main
 echo "Committed to Github!"
